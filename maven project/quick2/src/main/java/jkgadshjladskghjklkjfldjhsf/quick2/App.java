83fa5f12c5db06b848fb9d10d6ab9ba729b55d2e
banothuveeranna <99731863+banothuveeranna@users.noreply.github.com>
2022-07-18 10:33:18 +0530

    first commit

---
+++ maven project/quick2/src/main/java/jkgadshjladskghjklkjfldjhsf/quick2/App.java
@@ -0,0 +1,13 @@
+package jkgadshjladskghjklkjfldjhsf.quick2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
